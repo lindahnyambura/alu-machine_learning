8d3dcce391901a330f4c5da9e216123451e949c4
Lindah <l.nyambura@alustudent.com>
2024-10-05 20:31:40 +0300

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+"""
+Write a Python program that calculates the
+summation of all the elements in a list
+"""
 
-"""
-Write a Python program that calculates the summation of all the elements in a list
-"""
+
 def summation_i_squared(n):
+    """Calculates the summation of all
+    the elements in a list."""
     if not isinstance(n, int) or n < 1:
         return None
     return (n * (n + 1) * (2 * n + 1)) // 6
-

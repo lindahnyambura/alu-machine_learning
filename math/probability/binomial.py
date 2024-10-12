41472b6e9c007a0425f54cc72c1fffef2f292e68
Lindah <l.nyambura@alustudent.com>
2024-10-12 08:09:02 +0300

    binomial pmf

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -21,7 +21,7 @@ class Binomial:
 
         if data is None:
             if not isinstance(n, int) or n <= 0:
-                raise ValueError("n must be a positive integer")
+                raise ValueError("n must be a positive value")
             if not (0 < p < 1):
                 raise ValueError("p must be greater than 0 and less than 1")
             self.n = n
@@ -32,7 +32,7 @@ class Binomial:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            
+
             # calculate the mean
             mean = sum(data) / len(data)
 
@@ -44,3 +44,34 @@ class Binomial:
             self.n = round(mean / self.p)
 
             self.p = mean / self.n
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
+        """
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        binomial_coeff = self.binomial_coefficient(self.n, k)
+
+        pmf_value = binomial_coeff * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+        return pmf_value
+
+    def binomial_coefficient(self, n, k):
+        """
+        calculates the binomial coefficient
+        """
+        return self.factorial(n) / (self.factorial(k) * self.factorial(n - k))
+
+    def factorial(self, n):
+        """
+        calculates the factorial of a given number
+        """
+        if n == 0 or n == 1:
+            return 1
+        fact = 1
+        for i in range(2, n + 1):
+            fact *= i
+        return fact

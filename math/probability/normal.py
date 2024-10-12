8f65d3118ff4bc0d6da9564da1c369e3d9320d97
Lindah <l.nyambura@alustudent.com>
2024-10-12 07:35:45 +0300

    abnormal cdf

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This module contains the class Normal for
 modeling normal distributions
@@ -8,63 +7,54 @@ modeling normal distributions
 
 class Normal:
     """
-    represents a normal distribution
+    representing a normal distribution
     """
 
     def __init__(self, data=None, mean=0., stddev=1.):
         """
         initialization of class
         """
+
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
         if data is None:
-            if stddev <= 0:
-                raise ValueError("stddev must be a positive value")
-            else:
-                self.stddev = float(stddev)
-                self.mean = float(mean)
+            self.mean = float(mean)
+            self.stddev = float(stddev)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
-            mean = float(sum(data) / len(data))
-            self.mean = float(mean)
-            self.stddev = float(self.calculate_stddev(data, self.mean))
-
-    def calculate_stddev(self, data, mean):
-        """
-        calculates the standard deviation
-        """
-        variance = sum((x - mean) ** 2 for x in data) / len(data)
-        return (variance ** 0.5)
+            self.mean = float(sum(data) / len(data))
+            stddev_num = [(x - self.mean) ** 2 for x in data]
+            self.stddev = (sum(stddev_num) / len(data)) ** 0.5
 
     def z_score(self, x):
         """
-        calculates the z-score
+        returns the z-score of x
         """
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
         """
-        calculates the x-value
+        returns the x-value of z
         """
-        return (z * self.stddev) + self.mean
+        return self.mean + z * self.stddev
 
     def pdf(self, x):
-        """
-        calculates the value of the PDF for a given number
-        """
-        if self.stddev <= 0:
-            return 0
+        '''
+        returns the pdf
+        '''
         exponent = -((x - self.mean) ** 2) / (2 * (self.stddev ** 2))
-        coefficient = 1 / (self.stddev * (2 * 3.1415926536) ** 0.5)
-        return coefficient * (2.7182818285 ** exponent)
+        pdf_value = (1 / (self.stddev * ((2 * 3.1415926536) ** 0.5))) * \
+            (2.7182818285 ** exponent)
+        return pdf_value
 
     def cdf(self, x):
-        """
-        calculates the value of the CDF for a given number
-        """
-        z = self.z_score(x)
+        '''
+        returns the cdf
+        '''
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
         t = z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216
         cdf = 0.5 * (1 + (2 / (3.1415926536 ** 0.5)) * t)
         return cdf

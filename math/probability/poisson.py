1f27eecfa62d1d06fcc9e84615b417f54a766842
Lindah <l.nyambura@alustudent.com>
2024-10-12 03:32:22 +0300

    pycode style & 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+This module contains the class Poisson for modeling Poisson distributions
+"""
+
+
 class Poisson:
     """
     Poisson distribution
@@ -12,7 +17,7 @@ class Poisson:
 
         if lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
-        
+
         if data is None:
             self.lambtha = float(lambtha)
         else:
@@ -20,5 +25,37 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            
+
             self.lambtha = float(sum(data) / len(data))
+
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of successes
+        """
+
+        if k < 0:
+            return 0
+
+        # calculate the pmf 
+        lambtha = self.lambtha
+        e = 2.7182818285
+        pmf_value = ((lambtha ** k) * (e ** -lambtha)) / self.factorial(k)
+
+        return pmf_value
+
+
+    def factorial(self, n):
+        """
+        calculates the factorial of a given number
+        """
+
+        if n < 0:
+            return 0
+
+        # calculate the factorial
+        factorial_value = 1
+        for i in range(1, n + 1):
+            factorial_value *= i
+
+        return factorial_value
